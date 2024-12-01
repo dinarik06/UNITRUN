c3854b24200ef5187cc5def103fe718e75333ae0
dinarik06 <dinar.nurislamov.06@gmail.com>
2024-12-02 03:06:16 +0500

    Update settings.py

--- unitrun/settings.py
+++ unitrun/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sxw5iuk%%i1ro*v-gy8hla4i2%w7z^lv7^yycj8wut(=$#mici'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
